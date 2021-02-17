e55b1731aebefd0282a9fdd05047a25d9c43d298
Nina <>
2021-02-17 16:55:47 +0100

    simple node module

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // evaluate the perimeter and area of a rectangular
 
-var rect = {
-    perimeter: (x,y) => (2*(x+y)),
-    area: (x,y) => (x*y)
-};
+var rect = require('./rectangle');
 
 function solveRect(l,b) {
     console.log(`Solving for rectangle with l = ${l} and b = ${b}`);
